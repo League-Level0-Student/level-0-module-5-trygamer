ef6b8d64b35ccf6e0dab48bacc841923edf7150a
trygamer <league@WTS10.attlocal.net>
2018-03-02 18:25:06 -0800

    Guessing Pi.ip

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -2,14 +2,29 @@
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		int z=0;
+		
+		int x=0;
+		
+		int y=1;
+		
+		System.out.println(x);
+		System.out.println(y);
 		
 		for(int i=0;i<13;i++ ) {
 			
-	int test=0+i;
+			 z=x+y;
 			
 			
+
+	
+	x=y;
+	
+	y=z;
 			
-			System.out.println(test);
+			
+	System.out.println(z);
+			
 			
 		}
 		
--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -1,35 +1,57 @@
+import javax.swing.JOptionPane;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
  */
-
-import javax.swing.JOptionPane;
 
 public class GoofyNames {
 	public static void main(String[] args) {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String user = JOptionPane.showInputDialog(null, "What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
+		// Run your program to see that this works.
+
+		System.out.println(user.toUpperCase());
 
 		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
 
-			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
+		for (int i = 0; i < user.length(); i++) {
 
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
+			String h = user.substring(i, i + 1);
 
-		
+			if (i % 2 == 0) {
+				goofyName += h.toUpperCase();
+
+			}
+
+			else {
+
+				goofyName += h.toLowerCase();
+			}
+		}
+
+		JOptionPane.showMessageDialog(null, goofyName);
+
+		// HINT: Use .length() to determine the number of characters in the String.
+
+		// 4. Create a String variable to store the next character of the name
+		// using .substring(start, end)
+		// HINT: replace 'start' and 'end' to get String with character at i
+
+		// 5. Use MODULO to set this variable to upper case for EVEN characters
+		// and lower case for ODD characters.
+
+		// 6. ADD this String (containing 1 char) to the goofyName String
+
 		// 7. Use pop-up to show user their Goofy name
 
 	}
-}
 
+	private static void toUpperCase(String user) {
+		// TODO Auto-generated method stub
+
+	}
+}
